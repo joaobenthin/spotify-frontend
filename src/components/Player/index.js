a8a3ff08bb97ca4023f37d94ec8faa4da85efb01
joaobenthin <joaopaulobenthin@hotmail.com>
2019-06-22 21:44:25 -0300

    player created and styled

---
+++ src/components/Player/index.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import Slider from 'rc-slider'
+
+import {
+  Container,
+  Current,
+  Volume,
+  Progress,
+  Controls,
+  Time,
+  ProgressSlider
+} from './styles'
+
+import VolumeIcon from '../../assets/images/volume.svg'
+import SuffleIcon from '../../assets/images/shuffle.svg'
+import BackwardIcon from '../../assets/images/backward.svg'
+import PlayIcon from '../../assets/images/play.svg'
+import PauseIcon from '../../assets/images/pause.svg'
+import ForwardIcon from '../../assets/images/forward.svg'
+import RepeatIcon from '../../assets/images/repeat.svg'
+
+const Player = () => (
+  <Container>
+    <Current>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/pt/thumb/c/c5/Linkin_Park_Hunting_Party.jpg/220px-Linkin_Park_Hunting_Party.jpg"
+        alt="Linkin Park"
+      />
+
+      <div>
+        <span>Final Masquerade</span>
+        <small>Linkin Park</small>
+      </div>
+    </Current>
+
+    <Progress>
+      <Controls>
+        <button>
+          <img src={SuffleIcon} alt="Shuffle" />
+        </button>
+        <button>
+          <img src={BackwardIcon} alt="Backward" />
+        </button>
+        <button>
+          <img src={PlayIcon} alt="Play" />
+        </button>
+        <button>
+          <img src={ForwardIcon} alt="Forward" />
+        </button>
+        <button>
+          <img src={RepeatIcon} alt="Repeat" />
+        </button>
+      </Controls>
+
+      <Time>
+        <span>1:39</span>
+        <ProgressSlider>
+          <Slider
+            railStyle={{ background: '#404040', borderRadius: 10 }}
+            trackStyle={{ background: '#1ED760' }}
+            handleStyle={{ border: 0 }}
+          />
+        </ProgressSlider>
+        <span>4:24</span>
+      </Time>
+    </Progress>
+
+    <Volume>
+      <img src={VolumeIcon} alt="Volume" />
+      <Slider
+        railStyle={{ background: '#404040', borderRadius: 10 }}
+        trackStyle={{ background: '#FFF' }}
+        handleStyle={{ display: 'none' }}
+        value={100}
+      />
+    </Volume>
+  </Container>
+)
+
+export default Player
