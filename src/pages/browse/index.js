e38255505f184337ba47dff96aefab19d0af6774
joaobenthin <joaopaulobenthin@hotmail.com>
2019-06-30 17:54:03 -0300

    showing playlists in home

--- src/pages/browse/index.js
+++ src/pages/browse/index.js
@@ -1,46 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Creators as PlaylistsActions } from '../../store/ducks/playlists'
 
 import { Container, Title, List, Playlist } from './styles'
 
-const Browse = () => (
-  <Container>
-    <Title>Navegar</Title>
+class Browse extends Component {
+  static propTypes = {
+    getPlaylistsRequest: PropTypes.func.isRequired,
+    playlists: PropTypes.shape({
+      data: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        title: PropTypes.string,
+        description: PropTypes.string
+      }))
+    }).isRequired
+  }
 
-    <List>
-      <Playlist to="/playlists/1">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/pt/thumb/c/c5/Linkin_Park_Hunting_Party.jpg/220px-Linkin_Park_Hunting_Party.jpg"
-          alt="Playlist"
-        />
-        <strong>Podcast de Tecnologia</strong>
-        <p>Aprenda um pouco sobre o mundo da tecnologia</p>
-      </Playlist>
-      <Playlist to="/playlists/1">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/pt/thumb/c/c5/Linkin_Park_Hunting_Party.jpg/220px-Linkin_Park_Hunting_Party.jpg"
-          alt="Playlist"
-        />
-        <strong>Podcast de Tecnologia</strong>
-        <p>Aprenda um pouco sobre o mundo da tecnologia</p>
-      </Playlist>
-      <Playlist to="/playlists/1">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/pt/thumb/c/c5/Linkin_Park_Hunting_Party.jpg/220px-Linkin_Park_Hunting_Party.jpg"
-          alt="Playlist"
-        />
-        <strong>Podcast de Tecnologia</strong>
-        <p>Aprenda um pouco sobre o mundo da tecnologia</p>
-      </Playlist>
-      <Playlist to="/playlists/1">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/pt/thumb/c/c5/Linkin_Park_Hunting_Party.jpg/220px-Linkin_Park_Hunting_Party.jpg"
-          alt="Playlist"
-        />
-        <strong>Podcast de Tecnologia</strong>
-        <p>Aprenda um pouco sobre o mundo da tecnologia</p>
-      </Playlist>
-    </List>
-  </Container>
-)
+  componentDidMount () {
+    this.props.getPlaylistsRequest()
+  }
 
-export default Browse
+  render () {
+    return (
+      <Container>
+        <Title>Navegar</Title>
+
+        <List>
+          {this.props.playlists.data.map(playlist => (
+            <Playlist key={playlist.id} to={`/playlists/${playlist.id}`}>
+            <img
+              src={playlist.thumbnail}
+              alt={playlist.title}
+            />
+            <strong>{playlist.title}</strong>
+            <p>{playlist.description}</p>
+          </Playlist>
+          ))}
+        </List>
+      </Container>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  playlists: state.playlists
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators(PlaylistsActions, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Browse)
+
