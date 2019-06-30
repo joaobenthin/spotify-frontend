739cc8a7977f65fb8f7c4b05fc0e64900310dcdf
joaobenthin <joaopaulobenthin@hotmail.com>
2019-06-30 19:18:43 -0300

    playing music

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Creators as PlaylistsDetailsActions } from '../../store/ducks/playlistDetails.js'
+import { Creators as PlaylistsDetailsActions } from '../../store/ducks/playlistDetails'
+import { Creators as PlayerActions } from '../../store/ducks/player'
 
 import { Container, Header, SongList } from './styles'
 
@@ -34,6 +35,7 @@ class Playlist extends Component {
       }),
       loading: PropTypes.bool,
     }).isRequired,
+    loadSong: PropTypes.func.isRequired,
   }
 
   componentDidMount () {
@@ -91,7 +93,7 @@ class Playlist extends Component {
                 </tr>
               ) : (
                 playlist.songs.map(song => (
-                  <tr key={song.id}>
+                  <tr key={song.id} onDoubleClick={() => this.props.loadSong(song)}>
                     <td>
                       <img src={PlusIcon} alt="Adicionar" />
                     </td>
@@ -119,6 +121,6 @@ const mapStateToProps = state => ({
   playlistDetails: state.playlistDetails
 })
 
-const mapDispatchToProps = dispatch => bindActionCreators(PlaylistsDetailsActions, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ ...PlaylistsDetailsActions, ...PlayerActions }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Playlist)
